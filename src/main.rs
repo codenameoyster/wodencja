bdeb29dd77b9dcabd746f5bbd8ee681d0362cb57
Valery Piashchynski <piashchynski.valery@gmail.com>
2025-05-03 17:40:40 +0200

    feature: rustls sni parser
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,83 @@
-use axum::{Router, routing::get};
+use axum::{Router, http::Request, routing::get};
+use futures_util::pin_mut;
+use hyper::body::Incoming;
+use hyper_util::rt::{TokioExecutor, TokioIo};
+use openssl::ssl::{Ssl, SslAcceptor, SslFiletype, SslMethod};
+use std::{path::PathBuf, pin::Pin, sync::Arc, vec};
+use tokio::net::TcpListener;
+use tokio_openssl::SslStream;
+use tokio_rustls::TlsAcceptor;
+use tower::Service;
+use tracing::{error, info, warn};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+mod sni;
+
 #[tokio::main]
-async fn main() {}
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| format!("{}=debug", env!("CARGO_CRATE_NAME")).into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    // codenameoyster.ai-0001
+    // rustatian.me
+    _ = sni::init_cert_in_memory(vec![
+        "codenameoyster.ai-0001".to_string(),
+        "rustatian.me".to_string(),
+    ])
+    .await;
+
+    let config = Arc::new(sni::create_server_config());
+    let tls_acceptor = TlsAcceptor::from(config);
+    let bind = "[::1]:3000";
+    let tcp_listener = TcpListener::bind(bind).await.unwrap();
+    info!("HTTPS server listening on {bind}. To contact curl -k https://localhost:3000");
+    let app = Router::new().route("/", get(handler));
+
+    pin_mut!(tcp_listener);
+    loop {
+        let tower_service = app.clone();
+        let tls_acceptor = tls_acceptor.clone();
+
+        // Wait for new tcp connection
+        let (cnx, addr) = tcp_listener.accept().await.unwrap();
+
+        tokio::spawn(async move {
+            // Wait for tls handshake to happen
+            let Ok(stream) = tls_acceptor.accept(cnx).await else {
+                error!("error during tls handshake connection from {}", addr);
+                return;
+            };
+
+            // Hyper has its own `AsyncRead` and `AsyncWrite` traits and doesn't use tokio.
+            // `TokioIo` converts between them.
+            let stream = TokioIo::new(stream);
+
+            // Hyper also has its own `Service` trait and doesn't use tower. We can use
+            // `hyper::service::service_fn` to create a hyper `Service` that calls our app through
+            // `tower::Service::call`.
+            let hyper_service = hyper::service::service_fn(move |request: Request<Incoming>| {
+                // We have to clone `tower_service` because hyper's `Service` uses `&self` whereas
+                // tower's `Service` requires `&mut self`.
+                //
+                // We don't need to call `poll_ready` since `Router` is always ready.
+                tower_service.clone().call(request)
+            });
+
+            let ret = hyper_util::server::conn::auto::Builder::new(TokioExecutor::new())
+                .serve_connection_with_upgrades(stream, hyper_service)
+                .await;
+
+            if let Err(err) = ret {
+                warn!("error serving connection from {}: {}", addr, err);
+            }
+        });
+    }
+}
+
+async fn handler() -> &'static str {
+    "Hello, World!"
+}
---
+++ src/sni.rs
@@ -0,0 +1,103 @@
+use once_cell::sync::Lazy;
+use rustls::ServerConfig;
+use rustls::crypto::aws_lc_rs::Ticketer;
+use rustls::crypto::aws_lc_rs::sign::any_supported_type;
+use rustls::server::ServerSessionMemoryCache;
+use rustls::{compress::CompressionCache, server::ResolvesServerCert, sign::CertifiedKey};
+use std::sync::{Arc, Mutex};
+use std::{fs::File, io::BufReader};
+use tracing_subscriber::fmt::format;
+
+pub static CERT_DB: Lazy<Mutex<std::collections::HashMap<String, TlsCollection>>> =
+    Lazy::new(|| Mutex::new(std::collections::HashMap::new()));
+
+#[derive(Debug)]
+struct ResolveServerCert;
+
+impl ResolvesServerCert for ResolveServerCert {
+    fn resolve(
+        &self,
+        client_hello: rustls::server::ClientHello<'_>,
+    ) -> Option<Arc<rustls::sign::CertifiedKey>> {
+        match client_hello.server_name() {
+            Some(sni) => {
+                if let Some(cert_key) = CERT_DB.lock().unwrap().get(&sni.to_string()) {
+                    return Some(cert_key.certified_key.clone());
+                } else {
+                    return None;
+                }
+            }
+            None => {
+                return None;
+            }
+        }
+    }
+}
+
+pub struct TlsCollection {
+    pub certified_key: Arc<rustls::sign::CertifiedKey>,
+}
+
+pub async fn get_cert_key(domain: &str) -> Option<CertifiedKey> {
+    // todo: read letsencrypt certs from disk
+
+    let cert_file = format!("/etc/letsencrypt/live/{}/fullchain.pem", domain);
+    let key_file = format!("/etc/letsencrypt/live/{}/privkey.pem", domain);
+
+    let cert_path = std::path::Path::new(&cert_file);
+    let key_path = std::path::Path::new(&key_file);
+
+    if cert_path.exists() && cert_path.is_file() && key_path.exists() && key_path.is_file() {
+        let cert_file = &mut BufReader::new(File::open(cert_file).unwrap());
+        let private_key_file = &mut BufReader::new(File::open(key_file).unwrap());
+
+        let certs = rustls_pemfile::certs(cert_file)
+            .collect::<Result<Vec<_>, _>>()
+            .unwrap();
+        let private_key = rustls_pemfile::private_key(private_key_file).unwrap();
+
+        if private_key.is_some() {
+            let pk = private_key.unwrap().clone_key();
+            let certified_key = CertifiedKey {
+                cert: certs,
+                key: any_supported_type(&pk).unwrap(),
+                ocsp: None,
+            };
+
+            return Some(certified_key);
+        }
+    }
+
+    return None;
+}
+
+pub fn create_server_config() -> ServerConfig {
+    let mut server_config = ServerConfig::builder()
+        .with_no_client_auth()
+        .with_cert_resolver(Arc::new(ResolveServerCert));
+
+    server_config.ticketer = Ticketer::new().unwrap();
+    server_config.session_storage = ServerSessionMemoryCache::new(10024);
+    server_config.cert_compression_cache = Arc::new(CompressionCache::new(2038));
+    server_config.key_log = Arc::new(rustls::KeyLogFile::new());
+    server_config.max_early_data_size = 2048;
+    server_config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec()];
+    server_config
+}
+
+// codenameoyster.ai-0001
+// rustatian.me
+pub async fn init_cert_in_memory(listen: Vec<String>) -> std::io::Result<()> {
+    for domain in listen {
+        if let Some(key) = get_cert_key(&domain).await {
+            let mut cert_db = CERT_DB.lock().unwrap();
+            cert_db.insert(
+                domain.to_owned(),
+                TlsCollection {
+                    certified_key: Arc::new(key),
+                },
+            );
+        }
+    }
+    Ok(())
+}
